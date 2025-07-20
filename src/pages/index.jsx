a6a6b9929c80b4616a2acc0c1108de0486750b08
Caverna Tecnologica LLC <87895126+JuniorRoja5@users.noreply.github.com>
2025-07-20 19:44:02 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from "./Layout.jsx";
+import { LanguageProvider } from '../components/LanguageProvider'; // <-- IMPORTANTE: Importa el proveedor de idioma
 
 // Importamos todas las páginas que queremos poder ver
 import Dashboard from "./Dashboard";
@@ -11,7 +12,7 @@ import LinkPreview from "./LinkPreview";
 import Design from "./Design";
 import Customers from "./Customers";
 import Statistics from "./Statistics";
-import Orders from "./orders"; // 'orders' está en minúscula según tu archivo original
+import Orders from "./orders";
 import Calendar from "./Calendar";
 import Referrals from "./Referrals";
 import EmailSequences from "./EmailSequences";
@@ -22,33 +23,37 @@ import PublicProduct from "./PublicProduct";
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        {/* Aquí definimos todas las rutas posibles dentro de nuestra aplicación */}
-        <Routes>
-          {/* Ruta por defecto: si no se especifica nada, muestra el Dashboard */}
-          <Route path="/" element={<Dashboard />} />
-          
-          {/* Rutas para cada una de las páginas */}
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/PublicProfile" element={<PublicProfile />} />
-          <Route path="/LinkPreview" element={<LinkPreview />} />
-          <Route path="/Design" element={<Design />} />
-          <Route path="/Customers" element={<Customers />} />
-          <Route path="/Statistics" element={<Statistics />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/Calendar" element={<Calendar />} />
-          <Route path="/Referrals" element={<Referrals />} />
-          <Route path="/EmailSequences" element={<EmailSequences />} />
-          <Route path="/InstagramAutomation" element={<InstagramAutomation />} />
-          <Route path="/AskLink" element={<AskLink />} />
-          <Route path="/PublicStorefront" element={<PublicStorefront />} />
-          <Route path="/PublicProduct" element={<PublicProduct />} />
-        </Routes>
-      </Layout>
-    </Router>
+    // 👇 Paso 1: Envolvemos toda la aplicación con el LanguageProvider
+    <LanguageProvider>
+      <Router>
+        {/* El Layout ahora recibe los componentes de las páginas como "children" */}
+        <Layout>
+          {/* 👇 Paso 2: Definimos todas las rutas que se mostrarán dentro del Layout */}
+          <Routes>
+            {/* Ruta por defecto: si no se especifica nada, muestra el Dashboard */}
+            <Route path="/" element={<Dashboard />} />
+            
+            {/* Rutas para cada una de las páginas */}
+            <Route path="/Dashboard" element={<Dashboard />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/PublicProfile" element={<PublicProfile />} />
+            <Route path="/LinkPreview" element={<LinkPreview />} />
+            <Route path="/Design" element={<Design />} />
+            <Route path="/Customers" element={<Customers />} />
+            <Route path="/Statistics" element={<Statistics />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/Calendar" element={<Calendar />} />
+            <Route path="/Referrals" element={<Referrals />} />
+            <Route path="/EmailSequences" element={<EmailSequences />} />
+            <Route path="/InstagramAutomation" element={<InstagramAutomation />} />
+            <Route path="/AskLink" element={<AskLink />} />
+            <Route path="/PublicStorefront" element={<PublicStorefront />} />
+            <Route path="/PublicProduct" element={<PublicProduct />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </LanguageProvider>
   );
 }
 
